08a255e23f12c9acaf148dbef4a7f7bd5a7aa06d
Tushar Dahiya <tusharxoxoxo@gmail.com>
2022-03-30 00:30:58 +0530

    Time: 150 ms (39.30%), Space: 10.3 MB (67.06%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    int n = nums.size();
+    /*int n = nums.size();
         vector<int>answer;
         int i,j;i = 0; j = 0;
         for(int i = 0; i<n-1; ++i){
@@ -17,20 +17,36 @@ public:
         
         
         
-        return answer;
+        return answer;*/
         
         
-        /*vector<int> answer;
+       /* vector<int> answer;
         unordered_map<int, int> shark;
         
-        for(i = 0; i < nums.size(); ++i ){
-            if(target - nums[i] != shark.end()){
+        for(int i = 0; i < nums.size(); ++i ){
+            auto it = (target - nums[i]);
+            if(shark.find(it) != shark.end()){
                 answer.pushback(shark[target - nums[i]]);
                 answer.pushback(i);
             }
             shark[nums[i]] = i;
         }
-        return answer;*/
+        return answer;
+        
+        
+ */ 
+        int n = nums.size();
+        vector<int> ans;
+        for(int i = 0; i<n; ++i){
+        int a = target - nums[i];
+        auto it = find(nums.begin(), nums.end(),a);    
+        if(it != nums.end() && it - nums.begin() != i){ 
+            ans.push_back(i);
+            ans.push_back(it - nums.begin());
+            return ans;
+        }
+        }
+        return ans;
     }
 };
 
