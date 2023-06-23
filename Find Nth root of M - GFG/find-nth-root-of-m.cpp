f2b90893f8f35ae47f3097fa7c7c6626ee884c95
Tushar Dahiya <tusharxoxoxo@gmail.com>
2023-06-23 23:48:43 +0530

    Added solution - LeetHub

---
+++ Find Nth root of M - GFG/find-nth-root-of-m.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	int NthRoot(int n, int m)
+	{
+	    // Code here.
+	    int l = 0;
+	    int r = m;
+	    
+	    while(l <= r){
+	        int mid = (r+l)/2;
+	        
+	        if(pow(mid, n) == m) return mid;
+	        else if(pow(mid, n) > m){
+	          r = mid - 1;
+	        } 
+	        else 
+	        l = mid + 1;
+	    }
+	    
+	    return -1;
+	   
+	   //int l=0;
+    //     int r=m;
+        
+    //     while(l<=r){
+    //         int mid=(l+r)/2;
+            
+    //         if(pow(mid,n)==m){
+    //             return mid;
+    //         }
+    //         else if(pow(mid,n)>m){
+    //             r=mid-1;
+    //         }
+    //         else{
+    //             l=mid+1;
+    //         }
+    //     }
+    //     return -1;
+	}  
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		Solution ob;
+		int ans = ob.NthRoot(n, m);
+		cout << ans << "\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
